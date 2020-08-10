4fabcf3718e9157a8817f4f6edaf12b2b5672698
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-08-10 19:05:38 -0300

    fix: tempo do setTimeOut aumentado de 1000ms para 2000ms

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
+import Splash from './src/pages/Splash';
 import Main from './src/pages/Main';
 
 const App = () => {
+  const [timePassed, setTimePassed] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimePassed(true);
+    }, 2000);
+  }, []);
+
   return (
     <>
       <StatusBar
@@ -11,7 +20,7 @@ const App = () => {
         backgroundColor="#36213e"
         networkActivityIndicatorVisible
       />
-      <Main />
+      {!timePassed ? <Splash /> : <Main />}
     </>
   );
 };
